4d768488bfc3135aa2f414e1b31ecea03a7a852a
Parth-12pm <parthsmahadik12027@gmail.com>
2025-11-29 20:54:36 +0530

    another major changes

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.cdnlogo.com", // Allow all cdnlogo.com subdomains
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
